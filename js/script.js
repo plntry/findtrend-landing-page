a0911fe9e18ccacec02311ea885e4c112b0a97ce
plntry <nedilko7b@gmail.com>
2025-01-22 18:56:03 +0200

    added platforms section

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
   mobileMenuToggleBtn.addEventListener("click", function () {
     mobileMenu.classList.toggle("navbar__mobile-menu-items--active");
   });
+
+  // Handling Active Platform
+  const platforms = document.querySelectorAll(".platforms__list-item");
+
+  platforms.forEach((platform) => {
+    platform.addEventListener("click", () => {
+      if (!platform.classList.contains("platforms__list-item--active")) {
+        platforms.forEach((item) =>
+          item.classList.remove("platforms__list-item--active")
+        );
+        platform.classList.add("platforms__list-item--active");
+      }
+    });
+  });
 });
 
 // Change navbar bg color on scroll
