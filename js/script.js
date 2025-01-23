8c424a6f28fb94c49155a6ca79b7e4de1903cb5c
plntry <nedilko7b@gmail.com>
2025-01-23 21:41:35 +0200

    added packets checkbox logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+const updatePricing = () => {
+  const checkbox = document.querySelector("#packet-subscription");
+  const listItems = document.querySelectorAll(".packets__list-item");
+
+  const packetsPricing = {
+    personalFirstLevel: {
+      month: 8,
+      year: 80,
+    },
+    regular: {
+      month: 20,
+      year: 200,
+    },
+    personalSecondLevel: {
+      month: 48,
+      year: 500,
+    },
+  };
+
+  const isYearly = checkbox.checked;
+
+  listItems.forEach((item, index) => {
+    // Get the corresponding pricing key
+    const pricingKey = Object.keys(packetsPricing)[index];
+
+    if (pricingKey) {
+      const priceElement = item.querySelector(".packets__item-price");
+      const durationElement = item.querySelector(".packets__item-duration");
+
+      const price = isYearly
+        ? packetsPricing[pricingKey].year
+        : packetsPricing[pricingKey].month;
+      const duration = isYearly ? "/Year" : "/Month";
+
+      // Update the price and duration elements
+      priceElement.textContent = `$${price}`;
+      durationElement.textContent = duration;
+    }
+  });
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   // Toggle Mobile Menu
   const mobileMenuToggleBtn = document.querySelector(
@@ -22,6 +63,40 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  // Handling Active Packet Card
+  const packetCards = document.querySelectorAll(".packets__list-item");
+
+  packetCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      if (!card.classList.contains("packets__list-item--active")) {
+        packetCards.forEach((item) =>
+          item.classList.remove("packets__list-item--active")
+        );
+        card.classList.add("packets__list-item--active");
+      }
+    });
+  });
+
+  // Packet Checkbox Label Styles
+  const checkbox = document.querySelector("#packet-subscription");
+  const labels = document.querySelectorAll(".packets__checkbox-label");
+
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      labels[0].classList.remove("packets__checkbox-label--active");
+      labels[1].classList.add("packets__checkbox-label--active");
+    } else {
+      labels[1].classList.remove("packets__checkbox-label--active");
+      labels[0].classList.add("packets__checkbox-label--active");
+    }
+  });
+
+  // Updating Cards Pricing Data
+  updatePricing();
+
+  // Update pricing whenever the checkbox changes
+  checkbox.addEventListener("change", updatePricing);
 });
 
 // Change navbar bg color on scroll
